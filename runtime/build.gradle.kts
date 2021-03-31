a6c72371cd53ac8f15edb55feae1083a5499ca9e
Paul Latzelsperger <paul@beardyinc.com>
2021-03-31 16:40:27 +0200

    cleaned up runtime build file

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -1,13 +1,3 @@
-
-
-val infoModelVersion: String by project
-val jacksonVersion: String by project
-val jerseyVersion: String by project
-
-val securityType: String by rootProject.extra
-val iamType: String by rootProject.extra
-val configFs: String by rootProject.extra
-
 plugins {
     `java-library`
     id("application")
