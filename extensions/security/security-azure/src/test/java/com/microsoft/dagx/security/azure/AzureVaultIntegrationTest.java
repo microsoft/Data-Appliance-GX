12fd80ee9ad0416485f10e9375445bc689185452
Paul Latzelsperger <paul@beardyinc.com>
2021-04-15 17:46:59 +0200

    prevent @BeforeAll of integration tests from runnint when not on CI

--- extensions/security/security-azure/src/test/java/com/microsoft/dagx/security/azure/AzureVaultIntegrationTest.java
+++ extensions/security/security-azure/src/test/java/com/microsoft/dagx/security/azure/AzureVaultIntegrationTest.java
@@ -2,10 +2,10 @@ package com.microsoft.dagx.security.azure;
 
 import com.microsoft.dagx.spi.monitor.Monitor;
 import com.microsoft.dagx.spi.security.VaultResponse;
-import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,15 +13,27 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.Properties;
 
+import static com.microsoft.dagx.spi.util.ConfigurationFunctions.propOrEnv;
 import static org.junit.jupiter.api.Assertions.*;
 
+@EnabledIfEnvironmentVariable(named = "CI", matches = "true")
 public class AzureVaultIntegrationTest {
 
+    private static final String secretKey1 = "testkey1";
+    private static final String secretKey2 = "testkey2";
+    private static final String secretKey3 = "testkey3";
+    private static final String secretKey4 = "testkey4";
     private static AzureVault azureVault;
-    private String secretKey;
 
     @BeforeAll
     public static void setupAzure() throws IOException, URISyntaxException {
+        // this is necessary because the @EnabledIf... annotation does not prevent @BeforeAll to be called
+        var isCi = propOrEnv("CI", "false");
+        if (!Boolean.parseBoolean(isCi)) {
+            return;
+        }
+
+
         var resStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("azurecredentials.properties");
         var props = new Properties();
         props.load(resStream);
@@ -30,31 +42,35 @@ public class AzureVaultIntegrationTest {
         var tenantId = props.getProperty("dagx.vault.tenantid");
         var vaultName = props.getProperty("dagx.vault.name");
         var certfile = props.getProperty("dagx.vault.certificate");
-        var certPath= Thread.currentThread().getContextClassLoader().getResource(certfile).toURI().getPath();
+        var certPath = Thread.currentThread().getContextClassLoader().getResource(certfile).toURI().getPath();
 
         azureVault = AzureVault.authenticateWithCertificate(new LoggerMonitor(), clientId, tenantId, certPath, vaultName);
+
     }
 
-    @BeforeEach
-    void verifyAzureResourceGroup() {
-        secretKey = "testkey";
+    @AfterAll
+    static void verifyAzureResourceGroup() {
+        azureVault.deleteSecret(secretKey1);
+        azureVault.deleteSecret(secretKey2);
+        azureVault.deleteSecret(secretKey3);
+        azureVault.deleteSecret(secretKey4);
     }
 
     @Test
     void storeSecret() {
 
-        VaultResponse vaultResponse = azureVault.storeSecret(secretKey, "testvalue");
+        VaultResponse vaultResponse = azureVault.storeSecret(secretKey1, "testvalue");
 
         assertTrue(vaultResponse.success());
-        assertEquals("testvalue", azureVault.resolveSecret(secretKey));
+        assertEquals("testvalue", azureVault.resolveSecret(secretKey1));
     }
 
     @Test
     void storeSecret_overwrites() {
-        azureVault.storeSecret(secretKey, "value1");
-        azureVault.storeSecret(secretKey, "value2");
+        azureVault.storeSecret(secretKey2, "value1");
+        azureVault.storeSecret(secretKey2, "value2");
 
-        assertEquals("value2", azureVault.resolveSecret(secretKey));
+        assertEquals("value2", azureVault.resolveSecret(secretKey2));
     }
 
     @Test
@@ -64,9 +80,9 @@ public class AzureVaultIntegrationTest {
 
     @Test
     void resolveSecret() {
-        assertTrue(azureVault.storeSecret(secretKey, "someVal").success());
+        assertTrue(azureVault.storeSecret(secretKey3, "someVal").success());
 
-        assertEquals("someVal", azureVault.resolveSecret(secretKey));
+        assertEquals("someVal", azureVault.resolveSecret(secretKey3));
     }
 
     @Test
@@ -78,16 +94,11 @@ public class AzureVaultIntegrationTest {
 
     @Test
     void delete() {
-        azureVault.storeSecret(secretKey, "someval");
+        azureVault.storeSecret(secretKey4, "someval");
 
-        VaultResponse vr = azureVault.deleteSecret(secretKey);
+        VaultResponse vr = azureVault.deleteSecret(secretKey4);
         assertTrue(vr.success());
         assertNull(vr.error());
-    }
-
-    @AfterEach
-    void cleanup() {
-        azureVault.deleteSecret(secretKey);
     }
 
     private static class LoggerMonitor implements Monitor {
