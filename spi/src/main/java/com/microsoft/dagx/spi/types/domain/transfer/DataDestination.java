26ce0aa0a03a8a808f51389931a3e6cf8ae23b45
Paul Latzelsperger <paul@beardyinc.com>
2021-04-26 09:22:13 +0200

    removed ignore-type annotation

--- spi/src/main/java/com/microsoft/dagx/spi/types/domain/transfer/DataDestination.java
+++ spi/src/main/java/com/microsoft/dagx/spi/types/domain/transfer/DataDestination.java
@@ -1,11 +1,9 @@
 package com.microsoft.dagx.spi.types.domain.transfer;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 /**
  * A destination where data is sent.
  */
-@JsonIgnoreProperties("type")
 public interface DataDestination {
 
     String getType();
